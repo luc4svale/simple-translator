3395edbf3b681f734beb06df3f330681548882cc
Lucas Vale <alucasvalle@gmail.com>
2026-09-13 02:16:37 -0300

    refactor: adaptacao do Parser para suporte a Token e TokenType

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
   public static void main(String[] args) throws Exception {
-    String input = "289-85+0+69";
-    Scanner scan = new Scanner(input.getBytes());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
-    System.out.println(scan.nextToken());
+    String input = "89+508-7+99";
+    Parser p = new Parser(input.getBytes());
+    p.parse();
   }
 }
